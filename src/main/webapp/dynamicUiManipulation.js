1942d2b8fdb1ae4e35b4cec93c5ce8a218321958
Bo Henriksen <bomarconi@gmail.com>
2018-02-14 22:49:25 +0100

    Det er nu muligt at tilføje drenge og pige navne. Alle 3 lister opdateres.

--- src/main/webapp/dynamicUiManipulation.js
+++ src/main/webapp/dynamicUiManipulation.js
@@ -1,25 +1,68 @@
 
 var boys = ["Peter", "lars", "Ole"];
-var girls = ["Janne","hanne","Sanne"];
+var girls = ["Janne", "hanne", "Sanne"];
 
 
-var girlsAsString = girls.join("\n");
 
-var boysList = boys.map(function(name) { 
-    return "<ul>" + name + "</ul>"; 
-}).join("\n");
+//Laver en ny liste. Tilføjer ul rundt om navnet og linjeskift.
+function newBoyList() {
+    var boysList = boys.map(function (name) {
+        return "<ul>" + name + "</ul>";
+    }).join("\n");
+    return boysList;
+}
 
-var girlsList = girls.map(function(name) { 
-    return "<ul>" + name + "</ul>"; 
-}).join("\n");
+function girlsListNew() {
+    var girlsList = girls.map(function (name) {
+        return "<ul>" + name + "</ul>";
+    }).join("\n");
+    return girlsList;
+}
 
-document.getElementById("boys").innerHTML = boysList;
-document.getElementById("girls").innerHTML = girlsList;
-document.getElementById("all").innerHTML = girlsList + boysList;
+//Skriver listerne på index siden når siden loades.
+var boysList = document.getElementById("boys").innerHTML = newBoyList();
+var girlsList = document.getElementById("girls").innerHTML = girlsListNew();
+document.getElementById("all").innerHTML = allPeopleList();
 
+//Bruges til at lægge de to lister sammen.
+function allPeopleList() {
+    var boysList = document.getElementById("boys").innerHTML = newBoyList();
+    var girlsList = document.getElementById("girls").innerHTML = girlsListNew();
     
-    
-    
-    
-    
+    return allPeoples = boysList + girlsList;
+}
+
+//Uden parenteser på getImage pga det er en reference.
+document.getElementById("btns").onclick = getInputs;
+
+function getInputs(e) {
+
+    //Target får DOM id'et.
+    var btn = e.target.id;
+    if (btn === "addboy") {
+        
+        //Henter den indtastet værdi
+        let newBoy = document.getElementById("newboy").value;
+        //Tilføjer det nye navn nederst
+        boys.push(newBoy);
+        //Opdater listerne
+        document.getElementById("boys").innerHTML = newBoyList();
+        document.getElementById("all").innerHTML = allPeopleList();
+        
+    } else if (btn === "addgirl") {
+        
+        let newGirl = document.getElementById("newgirl").value;
+        girls.push(newGirl);
+        document.getElementById("girls").innerHTML = girlsListNew();
+        document.getElementById("all").innerHTML = allPeopleList();
+        
+    } else {
+        console.log("Fejl i getInput()");
+    }
+
+}
+
+
+
+
 
