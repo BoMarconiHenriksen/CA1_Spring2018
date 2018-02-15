36d2b7a3cfaa1a6d2652ee97a2a278c5e99cd4a4
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-02-15 20:41:55 +0100

    Nu ser noget af det pænere udNu ser det pænere ud både når man vil have et enkelt userid og en tabel med navne og numre

--- src/main/webapp/page4.js
+++ src/main/webapp/page4.js
@@ -14,9 +14,9 @@ var userNos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var userNo = document.getElementById("userNo");
 var getAllUsers = document.getElementById("getAllUsers");
 var showWhatYouveFetched = document.getElementById("show");
-var url;
 
-var listToShow=document.getElementById("liste");
+var list;
+
 //Creates a number select menu in the select tags id="userNo"
 for (var i = 0; i < userNos.length; i++) {
     var opt = document.createElement('option');
@@ -34,23 +34,17 @@ var getAll = getAllUsers.onclick = function () {
 
     fetch('https://jsonplaceholder.typicode.com/users/').then(function (response) {
         response.text().then(function (text) {
-
             var myJSON = text;
-
             var myObj = JSON.parse(myJSON);
-            for (x in myObj) {
-                var list;
-             list+= '<tr><td>'+myObj[x].name + ' </td><td>' + myObj[x].phone+'</td></tr>';
+            for (var x in myObj) {
+
+                list += '<tr><td>' + myObj[x].name + ' </td><td>' + myObj[x].phone + '</td></tr>';
             }
-            console.log(list);
-            listToShow.innerHTML='<tr><th>Name</th><th>Phone</th></tr>'+list+'';
+
+            showWhatYouveFetched.innerHTML = '<table><tr><th>Name</th><th>Phone</th></tr>' + list + '</table>';
         });
-
-
     });
-
-}
-;
+};
 
 var confirmFunction = confirm.onclick = function () {
     fetch('https://jsonplaceholder.typicode.com/users/' + userNo.value + '').then(function (response) {
