e14f93903d9e1daf542ae4ad56478ff897eb4430
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-02-15 14:01:48 +0100

    Nu mangler jeg bare at få det til at se pænt ud

--- src/main/webapp/page4.js
+++ src/main/webapp/page4.js
@@ -6,6 +6,8 @@
 
 
 
+/* global fetch */
+
 var confirm = document.getElementById("confirmUserNo");
 var userNos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var obtain = document.getElementById("obtainViafetch");
@@ -13,7 +15,7 @@ var obtain = document.getElementById("obtainViafetch");
 var userNo = document.getElementById("userNo");
 var getAllUsers = document.getElementById("getAllUsers");
 var showWhatYouveFetched =document.getElementById("show");
-
+var url ;
 //Creates a number select menu in the select tags id="userNo"
 for (var i = 0; i < userNos.length; i++) {
     var opt = document.createElement('option');
@@ -26,22 +28,33 @@ userNo.addEventListener("onChange", confirmFunction);
 confirm.addEventListener("click", confirmFunction);
 getAllUsers.addEventListener("click", getAll);
 
+
+function createNode(element) {
+      return document.createElement(element);
+  }
+
+  function append(parent, el) {
+    return parent.appendChild(el);
+  }
+
+
 var confirmFunction = confirm.onclick = function () {
-    fetch('https://jsonplaceholder.typicode.com/users/' + userNo.value + '');
-    
-console.log('https://jsonplaceholder.typicode.com/users/' + userNo.value + '');
-};
-console.log('https://jsonplaceholder.typicode.com/users/' + userNo.value + '');
+    fetch('https://jsonplaceholder.typicode.com/users/' + userNo.value + '').then(function(response) {
+  response.text().then(function(text) {
+    showWhatYouveFetched.innerHTML =text;
+  });
+})};
 
 
+  
 
+ 
+  
+  
 var getAll = getAllUsers.onclick = function () {
-
- 
-    showWhatYouveFetched.innerHTML= fetch('https://jsonplaceholder.typicode.com/users/');
-//      showWhatYouveFetched.appendChild( showWhatYouveFetched);
- 
-console.log('https://jsonplaceholder.typicode.com/users/');
-};
-console.log('https://jsonplaceholder.typicode.com/users/');
-
+url = 'https://jsonplaceholder.typicode.com/users/';
+fetch(url).then(function(response) {
+  response.text().then(function(text) {
+    showWhatYouveFetched.innerHTML =text;
+  });
+})};
