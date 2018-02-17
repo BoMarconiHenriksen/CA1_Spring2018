1371f03b98f26fd23d0ccf79e24d5aec0b3c8277
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-02-17 22:59:16 +0100

    jeg har arbejdet lidt med javascript calculatoren

---
+++ src/main/webapp/scriptCalculator.js
@@ -0,0 +1,44 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+document.getElementById("buttons").onclick = getEntry;
+var equals = document.getElementById("calculate");
+var entry = document.getElementsByClassName("t1").value;
+var entries = [];
+var result;
+
+
+equals.addEventListener('click', getResult);
+
+
+
+function  getEntry(e) {
+if (e.target.innerText==='='){
+    
+}else{
+    console.log(e.target.innerText);
+    document.getElementById("display").innerHTML = e.target.innerText;
+    entries.push(e.target.innerText);
+    console.log(entries);
+
+}
+}
+;
+
+function getResult() {
+    result = (entries.join('') + ' = ' + eval(entries.join('')));
+    document.getElementById("display").innerHTML = result;
+
+    console.log(result);
+    entries = [];
+
+
+
+}
+
+
+
